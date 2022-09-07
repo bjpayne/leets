c423ffbc940c2de37f445a2970d59502331e69c8
Benjamin <bj.payne@me.com>
2022-09-07 04:25:45 -0400

    Update Solution.php

--- app/ProductOfArrayExceptSelf/Solution.php
+++ app/ProductOfArrayExceptSelf/Solution.php
@@ -12,12 +12,14 @@ class Solution
     {
         $product = [1];
 
+        // Products of all pairs before index
         for ($i = 1; $i < count($nums); $i++) {
             $product[$i] = $nums[$i - 1] * $product[$i - 1];
         }
 
         $suffixProduct = 1;
 
+        // Products of all pairs after index
         for ($i = count($nums) - 1; $i >= 0; $i--) {
             $product[$i] = $product[$i] * $suffixProduct;
 
